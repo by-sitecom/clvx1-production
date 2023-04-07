52a77d8cd5c0e57bf55175d1a3e52890de0fd32e
Антон Добриян <a.dobrian@gmail.com>
2023-04-07 09:40:12 +0300

    add ul

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,25 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid p-6">
-      <div>
-        <h1>CLVX 1 is a mechanical scissors keyboard</h1>
-      </div>
+      <h1>CLVX 1 is a mechanical scissors keyboard</h1>
+
+      <p>
+        Clevetura LTD is a design house focused on a development of TouchOnKeys®
+        technology and products with it.
+      </p>
+      <ul>
+        <li>Key travel distance 1.2mm — great for laptop keyboard lovers</li>
+        <li>Quite, tactile and low-profile</li>
+        <li>Standard keys layout — easy to switch</li>
+      </ul>
+      <p>
+        Headquarter is located in Limassol, Cyprus with offices in Shenzhen and
+        Minsk. Clevetura was founded in 2017 and develops TouchOnKeys from the
+        beginning until now and beyond.
+      </p>
+
+      <h2>CLVX 1 is a mechanical scissors keyboard</h2>
+
       <p>
         Clevetura LTD is a design house focused on a development of TouchOnKeys®
         technology and products with it.
@@ -14,21 +30,9 @@ export default function Home() {
         Minsk. Clevetura was founded in 2017 and develops TouchOnKeys from the
         beginning until now and beyond.
       </p>
-      <div>
-        <h2>CLVX 1 is a mechanical scissors keyboard</h2>
-      </div>
-      <p>
-        Clevetura LTD is a design house focused on a development of TouchOnKeys®
-        technology and products with it.
-      </p>
-      <p>
-        Headquarter is located in Limassol, Cyprus with offices in Shenzhen and
-        Minsk. Clevetura was founded in 2017 and develops TouchOnKeys from the
-        beginning until now and beyond.
-      </p>
-      <div>
-        <h3>CLVX 1 is a mechanical scissors keyboard</h3>
-      </div>
+
+      <h3>CLVX 1 is a mechanical scissors keyboard</h3>
+
       <p>
         Clevetura LTD is a design house focused on a development of TouchOnKeys®
         technology and products with it.
