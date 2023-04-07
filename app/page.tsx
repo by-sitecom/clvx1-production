be95c221f089ec99c1143535e3239ef0444fcb3c
Антон Добриян <a.dobrian@gmail.com>
2023-04-07 09:14:02 +0300

    h1

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@ export default function Home() {
   return (
     <div className="grid p-6">
       <div>
-        <h1 className="m_h2">CLVX 1 is a mechanical scissors keyboard</h1>
+        <h1 className="m_h1">CLVX 1 is a mechanical scissors keyboard</h1>
       </div>
-      <p className="text-[1.25rem] leading-[2rem]">
+      <p className="mb-4 text-[1.25rem] leading-[2rem]">
         Clevetura LTD is a design house focused on a development of TouchOnKeys®
-        technology and products with it. Headquarter is located in Limassol,
-        Cyprus with offices in Shenzhen and Minsk. Clevetura was founded in 2017
-        and develops TouchOnKeys from the beginning until now and beyond.
+        technology and products with it.
+      </p>{" "}
+      <p className="mb-4 text-[1.25rem] leading-[2rem]">
+        Headquarter is located in Limassol, Cyprus with offices in Shenzhen and
+        Minsk. Clevetura was founded in 2017 and develops TouchOnKeys from the
+        beginning until now and beyond.
       </p>
     </div>
   );
